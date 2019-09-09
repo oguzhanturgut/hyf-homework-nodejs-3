4683fe3d7f818bd634fbf120dcc59ff741031c23
Oguzhan <oguzhanturgut@outlook.com>
2019-09-09 18:17:47 +0200

    Add step-3

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ const PORT = 3000;
 const users = [];
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/users', (req, res) => res.send(users));
+app.get('/user/:id', (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+  res.json(user);
+});
+app.post('/user', (req, res) => {
+  const user = { id: 0 };
+  users.push(user);
+  res.json(user);
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
